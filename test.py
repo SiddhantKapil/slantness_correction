8f48838e53e26ba65b290ea2aa2e94bb12296013
Siddhant Kapil <siddhant.kapil@nagarro.com>
2017-11-21 15:21:04 +0530

    added outputs

--- test.py
+++ test.py
@@ -3,7 +3,9 @@ import cv2
 import numpy as np
 
 # read and convert image to black and white
-img = cv2.imread("data/a02-000-s02-04.png")
+filename = "data/a02-000-s01-00.png"
+out = "outputs/"
+img = cv2.imread(filename)
 p = deslant_image.RotateAndDeslantImage()
 gray_image = cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2GRAY)
 _, im = cv2.threshold(gray_image, 160, 1, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
@@ -17,3 +19,6 @@ cv2.imshow("image", im)
 cv2.namedWindow('image1',cv2.WINDOW_NORMAL)
 cv2.imshow("image1", img)
 cv2.waitKey(0)
+
+# write output
+cv2.imwrite(out + filename.split("/")[1] , im*255)
